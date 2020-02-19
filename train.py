a1b5f8b53e2e8a30bab00301dfddf39c02d2d373
jbentleyh <45768739+jbentleyh@users.noreply.github.com>
2020-02-19 11:46:18 -0600

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import cv2
 import random
 import pickle
 import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
